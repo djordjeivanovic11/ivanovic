2bb181ff7ec7f8580f1c7141bc4f9c61e556f0fa
djordjeivanovic11 <djordjeivanovic65@gmail.com>
2024-10-27 22:25:22 -0400

    fixed some visual stuff

--- personal_web/src/components/Footer/Footer.tsx
+++ personal_web/src/components/Footer/Footer.tsx
@@ -3,25 +3,23 @@ import Link from 'next/link';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import { robotoMono } from '@/app/fonts/fonts';
 
-
 const Footer: React.FC = () => {
-
   return (
-    <footer className="py-6">
+    <footer className="py-4 sm:py-6">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className={`text-sm ${robotoMono.className} mb-4 md:mb-0`}>
+        <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+          <div className={`text-xs sm:text-sm ${robotoMono.className} mb-4 sm:mb-0 text-center sm:text-left`}>
             © {new Date().getFullYear()} Djordje Ivanovic. All rights reserved.
           </div>
-          <div className="flex space-x-4">
+          <div className="flex space-x-6 sm:space-x-4">
             <Link href="https://github.com/djordjeivanovic11" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400 transition-colors duration-300">
-              <FaGithub size={24} />
+              <FaGithub size={20} className="sm:w-6 sm:h-6" />
             </Link>
             <Link href="https://www.linkedin.com/in/djordje-ivanovic11" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400 transition-colors duration-300">
-              <FaLinkedin size={24} />
+              <FaLinkedin size={20} className="sm:w-6 sm:h-6" />
             </Link>
             <Link href="mailto:dorde_ivanovic@college.harvard.edu" className="hover:text-gray-400 transition-colors duration-300">
-              <FaEnvelope size={24} />
+              <FaEnvelope size={20} className="sm:w-6 sm:h-6" />
             </Link>
           </div>
         </div>
