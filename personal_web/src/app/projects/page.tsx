9d8ad83345ba509acc9d13f74649b94f863c7f21
djordjeivanovic11 <djordjeivanovic65@gmail.com>
2024-10-27 23:48:39 -0400

    add

--- personal_web/src/app/projects/page.tsx
+++ personal_web/src/app/projects/page.tsx
@@ -3,7 +3,7 @@ import AllProjects from '@/components/Contents/Projects/AllProjects';
 
 const ProjectsPage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="min-h-screen overflow-hidden bg-[#1c0101]">
       <AllProjects />
     </div>
   );
